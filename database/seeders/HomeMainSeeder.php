c26da7e26b69fb321af35206d4ab8fc245fbacb3
fikrimilano <fikrimilano1@gmail.com>
2022-11-24 14:56:46 +0700

    Add Home seeder

---
+++ database/seeders/HomeMainSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class HomeMainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $totalRecord = 1;
+        $data = [];
+        for ($x = 0; $x <= $totalRecord; $x++) {
+            $data[] = [
+                'status' => "default",
+                'description' => "Selamat datang di portofolio saya",
+            ];
+        }
+        DB::table('projects')->insert($data);
+    }
+}
